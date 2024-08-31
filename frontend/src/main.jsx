2bfc589882d8a894ed743502ddf5b94b1f1e2b25
Atishay180 <atishayjain8807@gmail.com>
2024-08-31 22:39:48 +0530

    authentication functionality added

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,22 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,  //to avoid refetching data when tab is changed
+    }
+  }
+})
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </BrowserRouter>
   </StrictMode>,
 )
