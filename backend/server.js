40e527a1f91f193b0576f802a9044a6ae4efebc8
Atishay180 <atishayjain8807@gmail.com>
2024-09-01 22:11:08 +0530

    chore: Update express.json() middleware to limit JSON payload size to 5mb and refactor Posts component to use react-query for data fetching

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ cloudinary.config({
 const app = express()
 const PORT = process.env.PORT || 8000
 
-app.use(express.json())  //to parse the incoming requests with JSON payloads
+app.use(express.json({limit: "5mb"}))  //to parse the incoming requests with JSON payloads
 app.use(express.urlencoded({ extended: true }))  //to parse the form data(url encoded)
 
 app.use(cookieParser()) //to parse the cookies
